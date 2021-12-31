20004baeda3e9f9072fe07a69042b59c936e83a9
zzk0 <zzk9460@gmail.com>
2021-12-31 11:30:34 +0800

    rename test directory -> ci && add examples

---
+++ oneflow-backend/ci/test_resnet50_oneflow/resnet50_oneflow/model.py
@@ -0,0 +1,37 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import oneflow as flow
+import oneflow.nn as nn
+from flowvision.models.resnet import resnet50
+
+
+class MyGraph(nn.Graph):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def build(self, *input):
+        return self.model(*input)
+
+
+if __name__ == "__main__":
+    image = flow.ones((1, 3, 224, 224))
+    model = resnet50(pretrained=True, progress=True)
+    model.eval()
+    graph = MyGraph(model)
+    out = graph(image)
+    flow.save(graph, "1/model")
---
+++ oneflow-backend/examples/images/README.md
@@ -0,0 +1,5 @@
+All images are from [Pexels](https://www.pexels.com/) and licensed under [CC0 License](https://creativecommons.org/publicdomain/zero/1.0/)
+
+
+- [cat.jpg](https://www.pexels.com/photo/white-and-black-cat-156934/)
+- [dog.jpg](https://www.pexels.com/photo/two-yellow-labrador-retriever-puppies-1108099/)

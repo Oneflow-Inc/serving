19315acf7d02c9f3f69d84175237e485e62f33eb
zzk0 <zzk9460@gmail.com>
2022-01-01 20:43:28 +0800

    update install dependency

--- ci/install_dependency.sh
+++ ci/install_dependency.sh
@@ -24,11 +24,11 @@ git clone https://github.com/Oneflow-Inc/oneflow --depth=1
 cd oneflow
 mkdir build
 cd build
-cmake .. -C ../cmake/caches/cn/cuda.cmake -DBUILD_CPP_API=ON -DBUILD_MONOLITHIC_LIBONEFLOW_CPP_SO=ON -DBUILD_SHARED_LIBS=OFF -DWITH_MLIR=ON
+cmake .. -C ../cmake/caches/cn/cuda.cmake -DBUILD_CPP_API=ON -DBUILD_MONOLITHIC_LIBONEFLOW_CPP_SO=ON -DBUILD_SHARED_LIBS=OFF -DWITH_MLIR=ON -G Ninja
 ninja
 
 # build oneflow-backend
-cd /ofserving/oneflow-backend
+cd /ofserving
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/install  -DTRITON_BACKEND_REPO_TAG=r21.10 -DTRITON_CORE_REPO_TAG=r21.10 -DTRITON_COMMON_REPO_TAG=r21.10 -G Ninja -DCMAKE_PREFIX_PATH=/oneflow/build/liboneflow_cpp/share -DTRITON_ENABLE_GPU=ON ..

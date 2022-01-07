9f3a25dbfab49a9da841620f4c71aca97386e907
zzk0 <zzk9460@gmail.com>
2022-01-07 16:04:19 +0800

    remove oneflow in test

--- ci/test_resnet50_oneflow/resnet50_oneflow/resnet50_test.py
+++ ci/test_resnet50_oneflow/resnet50_oneflow/resnet50_test.py
@@ -14,21 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import numpy as np
-import oneflow as flow
-import oneflow.nn as nn
 import argparse
+import numpy as np
 import tritonclient.http as httpclient
-from flowvision.models.resnet import resnet50
-
-
-class MyGraph(nn.Graph):
-  def __init__(self, model):
-      super().__init__()
-      self.model = model
-
-  def build(self, *input):
-      return self.model(*input)
 
 
 if __name__ == '__main__':
@@ -41,12 +29,7 @@ if __name__ == '__main__':
 
     triton_client = httpclient.InferenceServerClient(url='127.0.0.1:8000')
 
-    image = np.random.randn(1, 3, 224, 224).astype(np.float32)
-    model = resnet50(pretrained=True, progress=True)
-    model.eval()
-    graph = MyGraph(model)
-    flow_output = graph(flow.tensor(image)).numpy()
-
+    image = np.ones((1, 3, 224, 224)).astype(np.float32)
     inputs = []
     inputs.append(httpclient.InferInput('INPUT_0', image.shape, "FP32"))
     inputs[0].set_data_from_numpy(image, binary_data=True)
@@ -54,4 +37,5 @@ if __name__ == '__main__':
     outputs.append(httpclient.InferRequestedOutput('OUTPUT_0', binary_data=True))
     results = triton_client.infer(FLAGS.model, inputs=inputs, outputs=outputs)
     output_data0 = results.as_numpy('OUTPUT_0')
+    flow_output = np.load("oneflow_output.npy")
     assert np.allclose(flow_output, output_data0, rtol=1e-03, atol=1e-03)

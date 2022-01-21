f0ce16991556b0e3a6caf01d77bc7167904436cf
zzk0 <zzk9460@gmail.com>
2022-01-21 13:26:46 +0800

    update build.sh

--- ci/build/build.sh
+++ ci/build/build.sh
@@ -18,8 +18,8 @@ set -uex
 cd oneflow
 mkdir -p build
 cd build
-cmake .. -C ../cmake/caches/cn/cuda.cmake -DBUILD_CPP_API=ON -DBUILD_SHARED_LIBS=ON -DWITH_MLIR=ON -G Ninja
-ninja -j8
+cmake .. -C ../cmake/caches/cn/fast/cuda-75.cmake -DBUILD_CPP_API=ON -DBUILD_SHARED_LIBS=ON -DWITH_MLIR=ON -G Ninja
+ninja -j16
 
 export ONEFLOW_BUILD=$(pwd)
 export PYTHONPATH=$(pwd)/../python
@@ -34,7 +34,7 @@ cd ../../oneflow-backend
 mkdir -p build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/install  -DTRITON_BACKEND_REPO_TAG=r$TRITON_VERSION -DTRITON_CORE_REPO_TAG=r$TRITON_VERSION -DTRITON_COMMON_REPO_TAG=r$TRITON_VERSION -G Ninja -DCMAKE_PREFIX_PATH=$ONEFLOW_BUILD/liboneflow_cpp/share -DTRITON_ENABLE_GPU=ON ..
-ninja -j8
+ninja -j16
 
 # install flowvision, run export model
 cd ../../ci

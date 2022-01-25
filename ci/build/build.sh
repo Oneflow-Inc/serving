abac40457823dc312adfae9dcccf5725dce31e35
zzk0 <zzk9460@gmail.com>
2022-01-25 12:19:32 +0800

    update workflow: seperate build oneflow and backend

--- ci/build/build.sh
+++ ci/build/build.sh
@@ -34,17 +34,8 @@ cmake --build . -j 8
 export ONEFLOW_BUILD=$(pwd)
 export PYTHONPATH=$(pwd)/../python
 
-# build oneflow-backend
-git config --global http.proxy ${HTTP_PROXY}
-git config --global https.proxy ${HTTP_PROXY}
-cd ../../oneflow-backend
-mkdir -p build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/install  -DTRITON_BACKEND_REPO_TAG=r$TRITON_VERSION -DTRITON_CORE_REPO_TAG=r$TRITON_VERSION -DTRITON_COMMON_REPO_TAG=r$TRITON_VERSION -G Ninja -DCMAKE_PREFIX_PATH=$ONEFLOW_BUILD/liboneflow_cpp/share -DTRITON_ENABLE_GPU=ON ..
-ninja -j16
-
 # install flowvision, run export model
-cd ../../ci
+cd ${ONEFLOW_CI_SRC_DIR}/../ci
 pip3 install -r build/requirement.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 cd ./test/test_resnet50_oneflow/resnet50_oneflow
 python3 export_model.py

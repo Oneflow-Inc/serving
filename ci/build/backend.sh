89be7469278fb14d242aad32e1f356892bcdda07
zzk0 <zzk9460@gmail.com>
2022-01-26 10:26:38 +0800

    update workflow

--- ci/build/backend.sh
+++ ci/build/backend.sh
@@ -2,7 +2,6 @@
 set -euxo pipefail
 
 # install flowvision, run export model
-python3 -m pip install --find-links=https://oneflow-staging.oss-cn-beijing.aliyuncs.com/canary/refs/heads/master/cu112/index.html oneflow
 cd ./ci
 pip3 install -r build/requirement.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 cd ./test/test_resnet50_oneflow/resnet50_oneflow

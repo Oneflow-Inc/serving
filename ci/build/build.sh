3d74047e5e6f044248125ee9c1e5a0d28fd5c565
ZeKai Zhou <30856589+zzk0@users.noreply.github.com>
2022-01-28 09:46:09 +0800

    ci use build cache (#8)
    * update workflow; use cache
    
    * update workflow
    
    * update workflow: seperate build oneflow and backend
    
    * update workflow: set use self-hosted
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * update cmake
    
    * update cmake, set release
    
    * update workflow
    
    * update build.sh
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * refine
    
    * update workflow
    
    * update cmake cache
    
    * update workflowk, add build.sh
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * update workflow: remove build python
    
    * set oneflow to cpu
    
    * use oss oneflow python with mlir
    
    * update workflow
    
    * Update test.yml
    
    * Update mlir-cuda-75.cmake
    
    * Update mlir-cuda-75.cmake
    
    * update workflow, add pip3 index-url
    
    * update workflow: remove index-url
    
    * update workflow: install oneflow no index
    
    * update script: install oneflow first then flowvision
    
    * update worflow: install all dependency inside scripts
    
    * update workflow: recover oneflow build
    
    * update workflow: install flowvision first
    
    * update workflow: install flowvision first then oneflow.whl
    
    * update workflow
    
    * update workflow: set flowvision==0.0.54
    
    * update workflow: fix dir
    
    * update workflow: fix missing TRITON_VERSION
    
    * update workflow: pass build stage temporarily
    
    * update workflow: pass build stage temporarily
    
    * update workflow: recover build stage
    
    * update workflow: pass build stage temporarily && fix build-cache-dir
    
    * update workflow: fix build-cache-dir
    
    * update workflow: build oneflow-backend in manylinux
    
    * update workflow: run python in triton-devel not in manylinux
    
    * update workflow: remove copy from manylinux
    
    * update workflow: run docker
    
    * update workflow: fix command
    
    * update workflow: recover oneflow build stage
    
    * update workflow: fix workdir
    
    * update workflow: disable auditwheel
    
    * update workflow: remove container
    
    * update workflow: install whl directly from oneflow/python/dist
    
    * update workflow: use PYTHONPATH to find oneflow
    
    * update workflow: recover build.sh
    
    * update filename && use less core
    
    Co-authored-by: tsai <jackalcooper@gmail.com>

--- ci/build/build.sh
+++ ci/build/build.sh
@@ -2,26 +2,32 @@
 set -euxo pipefail
 
 # build oneflow
-cd oneflow
-mkdir -p build
-cd build
-cmake .. -C ../cmake/caches/cn/cuda.cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_CPP_API=ON -DBUILD_SHARED_LIBS=ON -DWITH_MLIR=ON -G Ninja
-ninja -j16
+gcc --version
+ld --version
 
-export ONEFLOW_BUILD=$(pwd)
-export PYTHONPATH=$(pwd)/../python
+# clean python dir
+cd ${ONEFLOW_CI_SRC_DIR}
+${ONEFLOW_CI_PYTHON_EXE} -m pip install -i https://mirrors.aliyun.com/pypi/simple --user -r ci/fixed-dev-requirements.txt
+cd python
+mkdir dist
+git clean -nXd -e \!dist -e \!dist/**
+git clean -fXd -e \!dist -e \!dist/**
 
-# build oneflow-backend
-git config --global http.proxy ${HTTP_PROXY}
-git config --global https.proxy ${HTTP_PROXY}
-cd ../../oneflow-backend
-mkdir -p build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/install  -DTRITON_BACKEND_REPO_TAG=r$TRITON_VERSION -DTRITON_CORE_REPO_TAG=r$TRITON_VERSION -DTRITON_COMMON_REPO_TAG=r$TRITON_VERSION -G Ninja -DCMAKE_PREFIX_PATH=$ONEFLOW_BUILD/liboneflow_cpp/share -DTRITON_ENABLE_GPU=ON ..
-ninja -j16
+# cmake config
+mkdir -p ${ONEFLOW_CI_BUILD_DIR}
+cd ${ONEFLOW_CI_BUILD_DIR}
+find ${ONEFLOW_CI_BUILD_DIR} -name CMakeCache.txt
+find ${ONEFLOW_CI_BUILD_DIR} -name CMakeCache.txt -delete
+if [ ! -f "$ONEFLOW_CI_CMAKE_INIT_CACHE" ]; then
+    echo "$ONEFLOW_CI_CMAKE_INIT_CACHE does not exist."
+    exit 1
+fi
+cmake -S ${ONEFLOW_CI_SRC_DIR} -C ${ONEFLOW_CI_CMAKE_INIT_CACHE} -DPython3_EXECUTABLE=${ONEFLOW_CI_PYTHON_EXE}
+# cmake build
+cd ${ONEFLOW_CI_BUILD_DIR}
+cmake --build . -j8
 
-# install flowvision, run export model
-cd ../../ci
-pip3 install -r build/requirement.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-cd ./test/test_resnet50_oneflow/resnet50_oneflow
-python3 export_model.py
+# build pip
+cd ${ONEFLOW_CI_SRC_DIR}
+cd python
+${ONEFLOW_CI_PYTHON_EXE} setup.py bdist_wheel

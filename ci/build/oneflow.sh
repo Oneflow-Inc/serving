0a3eaf7c5589519b88af10c0ee5603db2949e396
mosout <mosout@qq.com>
2022-02-18 15:53:23 +0800

    fix

--- ci/build/oneflow.sh
+++ ci/build/oneflow.sh
@@ -2,17 +2,34 @@
 set -euxo pipefail
 
 flag_file="$ONEFLOW_CI_BUILD_DIR/done"
+oneflow_head=$(git -C $ONEFLOW_CI_SRC_DIR rev-parse HEAD)
+whl_src_dir=$ONEFLOW_CI_SRC_DIR/python/dist
+
+build_oneflow() {
+    rm -rf $WHEELHOUSE_DIR
+    mkdir -p $WHEELHOUSE_DIR
+    /bin/bash ${ONEFLOW_CI_BUILD_SCRIPT}
+    echo "$oneflow_head" > $flag_file
+    whls=$(ls $whl_src_dir)
+    whls_arr=(${whls// /})
+    whls_len=${#whls_arr[*]}
+    if [ $whls_len == 1 ]; then
+        auditwheel repair $whl_src_dir/whls --wheel-dir $WHEELHOUSE_DIR
+    else
+        echo "Please clean $whl_src_dir first"
+        exit 1
+    fi
+}
 
 if [ ! -f $flag_file ]; then
-    /bin/bash ${ONEFLOW_CI_BUILD_SCRIPT}
-    git -C $ONEFLOW_CI_SRC_DIR rev-parse HEAD > $flag_file 
+    build_oneflow
 else
     oneflow_head_built=$(<$flag_file)
-    oneflow_head=$(git -C $ONEFLOW_CI_SRC_DIR rev-parse HEAD)
     if [ "$oneflow_head_built" != "$oneflow_head" ]; then
-        /bin/bash ${ONEFLOW_CI_BUILD_SCRIPT}
-        echo "$oneflow_head" > $flag_file
+        build_oneflow
     else
+        cached_whl=$(ls $WHEELHOUSE_DIR)
+        pip3 install $WHEELHOUSE_DIR/$cached_whl
         echo "Use build cache for oneflow."
     fi
 fi

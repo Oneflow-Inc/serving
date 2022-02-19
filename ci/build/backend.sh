d1d5ccf6173d90442bf42a0983011ca03a8e4b63
mosout <mosout@qq.com>
2022-02-19 20:34:36 +0800

    support aliyun mirror for building backend

--- ci/build/backend.sh
+++ ci/build/backend.sh
@@ -7,5 +7,9 @@ git config --global https.proxy ${HTTP_PROXY}
 
 mkdir -p build
 cd build
-cmake -DCMAKE_PREFIX_PATH=$ONEFLOW_CI_BUILD_DIR/liboneflow_cpp/share -DTRITON_RELATED_REPO_TAG=r$TRITON_VERSION -DTRITON_ENABLE_GPU=ON -G Ninja ..
+cmake -DCMAKE_PREFIX_PATH=$ONEFLOW_CI_BUILD_DIR/liboneflow_cpp/share \
+    -DTRITON_RELATED_REPO_TAG=r$TRITON_VERSION \
+    -DTRITON_ENABLE_GPU=ON \
+    -DTHIRD_PARTY_MIRROR=aliyun \
+    -G Ninja ..
 ninja -j8

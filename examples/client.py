a21f70c43d707364e1adcf8acf7110b000790cb2
Mosout <mosout@qq.com>
2024-01-04 06:58:13 +0000

    fix

--- examples/client.py
+++ examples/client.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import time
 import argparse
 import numpy as np
@@ -28,7 +44,7 @@ if __name__ == '__main__':
     outputs = []
     outputs.append(httpclient.InferRequestedOutput('OUTPUT_0', binary_data=True, class_count=3))
     now = time.time()
-    results = triton_client.infer("resnet50_python", inputs=inputs, outputs=outputs)
+    results = triton_client.infer("resnet50", inputs=inputs, outputs=outputs)
     print(f"time cost: {time.time() - now}s")
     output_data0 = results.as_numpy('OUTPUT_0')
     print(output_data0)

14bb625a5ee7a6c706d4ca042c01eadafb8b0c05
JiangDongHua <jiangdonghua@oneflow-28>
2024-01-05 09:34:13 +0000

    [bugfix] fix entrypoint python path bug

--- ci/build/oneflow_serving_triton_entrypoint.sh
+++ ci/build/oneflow_serving_triton_entrypoint.sh
@@ -43,8 +43,8 @@ echo
 
 # This script can either be a wrapper around arbitrary command lines,
 # or it will simply exec bash if no arguments were given
+export PYTHONPATH=/opt/oneflow/python:$PYTHONPATH
 if [[ $# -eq 0 ]]; then
-  export PYTHONPATH=/opt/oneflow/python:$PYTHONPATH
   exec oneflow-serving --model-store /models
 else
   exec "$@"

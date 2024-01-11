ad5287a4f9d7269f2bf26fbe9da596e266c7b22a
Peihong Liu <mosout@qq.com>
2024-01-11 14:45:19 +0800

    SD backend (#35)
    * init
    
    * [bugfix] fix entrypoint python path bug
    
    * [feature] add sd backend, example and dockerfile
    
    ---------
    
    Co-authored-by: JiangDongHua <jiangdonghua@oneflow-28>

---
+++ projects/sd/oneflow_sd_serving_triton_entrypoint.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Copyright (c) 2016-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+# OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Gather parts in alpha order
+shopt -s nullglob extglob
+SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+declare -a PARTS=( "${SCRIPT_DIR}/entrypoint.d"/*@(.txt|.sh) )
+shopt -u nullglob extglob
+
+# Execute the entrypoint parts
+for file in "${PARTS[@]}"; do
+  case "${file}" in
+    *.txt) cat "${file}";;
+    *.sh)  source "${file}";;
+  esac
+done
+
+echo
+
+# This script can either be a wrapper around arbitrary command lines,
+# or it will simply exec bash if no arguments were given
+if [[ $# -eq 0 ]]; then
+  echo "You need to provide a huggingface token to download the model."
+  exit 0
+fi
+
+/miniconda3/envs/py310/bin/python3 -c "import sys;from huggingface_hub.hf_api import HfFolder; HfFolder.save_token(sys.argv[1])" "$1"
+if [[ $# -eq 1 ]]; then
+  exec oneflow-serving --model-store /models
+else
+  exec "${@:2}"
+fi

f9724a39369af96af0ad6e38ca0dca51ad3f02ce
Mosout <mosout@qq.com>
2024-12-18 14:56:42 +0800

    fix

--- ci/build/backend.sh
+++ ci/build/backend.sh
@@ -2,8 +2,8 @@
 set -euxo pipefail
 
 # build oneflow-backend
-git config --global http.proxy ${HTTP_PROXY}
-git config --global https.proxy ${HTTP_PROXY}
+# git config --global http.proxy ${HTTP_PROXY}
+# git config --global https.proxy ${HTTP_PROXY}
 
 mkdir -p build
 cd build
@@ -11,5 +11,7 @@ cmake -DCMAKE_PREFIX_PATH=$ONEFLOW_CI_BUILD_DIR/liboneflow_cpp/share \
     -DTRITON_RELATED_REPO_TAG=r$TRITON_VERSION \
     -DTRITON_ENABLE_GPU=ON \
     -DTHIRD_PARTY_MIRROR=aliyun \
+    -DBUILD_ONEFLOW_BACKEND=ON \
+    -DBUILD_ONEFLOW_LITE_BACKEND=OFF \
     -G Ninja ..
 ninja -j8

22be79c0462515e9248ab09f1b82e52d7251bd03
mosout <mosout@qq.com>
2024-12-24 11:09:34 +0800

    refine

--- ci/build/oneflow-serving.py
+++ ci/build/oneflow-serving.py
@@ -102,7 +102,8 @@ class OneFlowServing(object):
     def prepare(self):
         self._parse()
         
-        self._unknown.extend(['--disable-auto-complete-config'])
+        # self._unknown.extend(['--disable-auto-complete-config'])
+        self._unknown.extend(['--strict-model-config', 'false'])
         self._unknown_split = []
         for argument in self._unknown:
             self._unknown_split.extend(argument.split('='))

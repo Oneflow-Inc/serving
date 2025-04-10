a452db7a0304325883cf35b11d5d78dda222b25e
Mosout <mosout@qq.com>
2025-04-10 15:18:41 +0800

    add lite test

---
+++ test/test_lite/test.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+export CUDA_VISIBLE_DEVICES=0
+
+rm -rf ./models
+mkdir -p models/resnet50/1
+cp -r ../common/model models/resnet50/1/
+
+# generate minimal config.pbtxt
+echo "name: \"resnet50\"" >> models/resnet50/config.pbtxt
+echo "backend: \"oneflow_lite\"" >> models/resnet50/config.pbtxt
+
+SERVER=/opt/tritonserver/bin/tritonserver
+SERVER_ARGS="--model-repository=`pwd`/models --log-verbose=1 --strict-model-config false"
+SERVER_LOG="./inference_server.log"
+source ../common/util.sh
+
+run_server
+if [ "$SERVER_PID" == "0" ]; then
+    echo -e "\n***\n*** Failed to start $SERVER\n***"
+    cat $SERVER_LOG
+    exit 1
+fi
+
+echo "running resnet50 basic test with oneflow lite backend"
+python3 ../common/test_model.py --model resnet50 --target-output ../common/resnet50_output.npy
+
+kill $SERVER_PID
+wait $SERVER_PID
+
+exit 0
